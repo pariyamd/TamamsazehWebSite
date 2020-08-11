e7ca46f5af412f910bc891ecf772208786af8dcc
pariyamd <pa.mehrbod@gmail.com>
2020-08-11 10:51:35 +0000

    settings in server version

--- TamamsazehWebSite/settings.py
+++ TamamsazehWebSite/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'TamamsazehWebSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tamamsazehdb',
+        'USER': 'dbadmin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -123,7 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
